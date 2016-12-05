890830989cd529af98bb853631eba41580475dfa
Ryo Utsunomiya <millionsnowliving@gmail.com>
2016-12-05 21:55:21 +0900

    Fix command

--- src/AppBundle/Command/MongoDBTestCommand.php
+++ src/AppBundle/Command/MongoDBTestCommand.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Command;
 
 use AppBundle\Document\Product;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -23,20 +24,17 @@ class MongoDBTestCommand extends ContainerAwareCommand
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-//        $product = new Product();
-//        $product->setName('Foobar');
-//        $product->setPrice(19.99);
-//
-//        $dm = $this->getContainer()->get('doctrine_mongodb')->getManager();
-//        $dm->persist($product);
-//        $dm->flush();
-//
-//        var_dump($product->getId());
+        $product = new Product();
+        $product->setName('Foobar');
+        $product->setPrice(19.99);
+
+        /** @var DocumentManager $dm */
+        $dm = $this->getContainer()->get('doctrine_mongodb')->getManager();
+        $dm->persist($product);
+        $dm->flush();
         
-        $product = $this->getContainer()
-            ->get('doctrine_mongodb')
-            ->getRepository('AppBundle:Product')
-            ->findOneBy(['name' => 'Foobar']);
+        /** @var Product $product */
+        $product = $dm->getRepository('AppBundle:Product')->findOneBy(['name' => 'Foobar']);
         
         if (!$product) {
             $output->writeln('Product not found');
